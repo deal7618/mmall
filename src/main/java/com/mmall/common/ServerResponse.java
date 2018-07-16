f62cce6309efc565516249406a2bb2b6f6b1c060
deal <13634842428@163.com>
2018-07-16 18:33:08 +0800

    test1

---
+++ src/main/java/com/mmall/common/ServerResponse.java
@@ -0,0 +1,127 @@
+package com.mmall.common;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+import java.io.Serializable;
+
+@JsonSerialize(include =  JsonSerialize.Inclusion.NON_NULL)
+//保证序列化json的时候,如果是null的对象,key也会消失
+public class ServerResponse<T> implements Serializable {
+
+    private int status;
+    private String msg;
+    private T data;
+
+    private ServerResponse(int status){
+        this.status = status;
+    }
+    private ServerResponse(int status,T data){
+        this.status = status;
+        this.data = data;
+    }
+
+    private ServerResponse(int status,String msg,T data){
+        this.status = status;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    private ServerResponse(int status,String msg){
+        this.status = status;
+        this.msg = msg;
+    }
+
+    @JsonIgnore
+    //使之不在json序列化结果当中
+    public boolean isSuccess(){
+        return this.status == ResponseCode.SUCCESS.getCode();
+    }
+
+    public int getStatus(){
+        return status;
+    }
+    public T getData(){
+        return data;
+    }
+    public String getMsg(){
+        return msg;
+    }
+
+
+    /**
+     * 根据状态返回响应
+     * @param <T>
+     * @return
+     */
+    public static <T> ServerResponse<T> createBySuccess(){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode());
+    }
+
+    /**
+     * 跟据描述返回响应
+     * @param msg
+     * @param <T>
+     * @return
+     */
+    public static <T> ServerResponse<T> createBySuccessMessage(String msg){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg);
+    }
+
+    /**
+     * 跟据数据生成响应
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T> ServerResponse<T> createBySuccess(T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),data);
+    }
+
+    /**
+     * 跟据状态、信息和数据返回响应
+     * @param msg
+     * @param data
+     * @param <T>
+     * @return
+     */
+    public static <T> ServerResponse<T> createBySuccess(String msg,T data){
+        return new ServerResponse<T>(ResponseCode.SUCCESS.getCode(),msg,data);
+    }
+
+    /**
+     * 错误响应：状态码+错误描述
+     * @param <T>
+     * @return
+     */
+    public static <T> ServerResponse<T> createByError(){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
+    }
+
+    /**
+     *错误响应：状态码+错误类型
+     * @param errorMessage
+     * @param <T>
+     * @return
+     */
+    public static <T> ServerResponse<T> createByErrorMessage(String errorMessage){
+        return new ServerResponse<T>(ResponseCode.ERROR.getCode(),errorMessage);
+    }
+
+    public static <T> ServerResponse<T> createByErrorCodeMessage(int errorCode,String errorMessage){
+        return new ServerResponse<T>(errorCode,errorMessage);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
